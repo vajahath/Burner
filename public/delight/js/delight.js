a7f94ea6fb02ae3e6d4e19b2099406b2e2016d71
Vajahath Ahmed <vajuoff.1@gamil.com>
2016-07-08 17:32:09 +0530

    page completed including pricing

--- public/delight/js/delight.js
+++ public/delight/js/delight.js
@@ -31,3 +31,14 @@
     })
 
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
