958785dc61028f0047f808344f0e432ebf52d515
Vajahath Ahmed <vajuoff.1@gamil.com>
2016-07-20 14:13:49 +0530

    started developing drawer with workflow

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {});
 });
-
-
 
 router.get('/delight', function(req, res){
 	res.render('delight', {});
@@ -16,6 +15,12 @@ router.get('/register', function(req, res){
 	res.render('register', {});
 });
 
+router.get('/wdf', function(req, res){
+	res.sendFile('Work_Flow.html', { root: path.join(__dirname, '../views') });
+});
 
+router.get('/dafaq', function(req, res){
+	res.sendFile('drawer.html', { root: path.join(__dirname, '../views') });
+});
 
 module.exports = router;
