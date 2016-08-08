9f05d7d9854e927328aaa310e34bf75aaf90c116
Vajahath Ahmed <vajuoff.1@gamil.com>
2016-08-08 17:35:33 +0530

    landing completed :)

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -3,6 +3,17 @@
 
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
+		$('.carousel').carousel({
+			// time_constant:1000
+		});
+		var options = [{
+			selector: '#sosoft-1',
+			offset: 320,
+			callback: function(el) {
+				$('.carousel').carousel('next', 2);
+			}
+		}];
+		Materialize.scrollFire(options);
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
