973eedecccd91154ea10d8e0dd2b1752aec903ee
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-11-01 14:14:11 +0530

    properly formtd

--- public/main/products/cms/js/delight.js
+++ public/main/products/cms/js/delight.js
@@ -1,51 +1,51 @@
 // Agency Theme JavaScript
 
 (function($) {
-    "use strict"; // Start of use strict
+	"use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		}, 1250, 'easeInOutExpo');
+		event.preventDefault();
+	});
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+		target: '.navbar-fixed-top',
+		offset: 51
+	});
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+		$('.navbar-toggle:visible').click();
+	});
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    });
+	// Offset for Main Navigation
+	$('#mainNav').affix({
+		offset: {
+			top: 100
+		}
+	});
 
-    $('#cms-download').click(function() {
-        window.location.href = 'downloadables/cms.pdf';
-    });
-    $('#sms-download').click(function() {
-        window.location.href = 'downloadables/sms.pdf';
-    });
+	$('#cms-download').click(function() {
+		window.location.href = 'downloadables/cms.pdf';
+	});
+	$('#sms-download').click(function() {
+		window.location.href = 'downloadables/sms.pdf';
+	});
 
 })(jQuery); // End of use strict
 
 $(document).ready(function() {
-    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
-        e.preventDefault();
-        $(this).siblings('a.active').removeClass("active");
-        $(this).addClass("active");
-        var index = $(this).index();
-        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-    });
+	$("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('a.active').removeClass("active");
+		$(this).addClass("active");
+		var index = $(this).index();
+		$("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+		$("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+	});
 });
